475e1c09466daee0fe494889930941ef8ee63228
chansooo <kcs05008@gmail.com>
2022-04-28 01:21:24 +0900

    feature-readFile

---
+++ src/LexicalAnalyzer.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.nio.Buffer;
+
+public class LexicalAnalyzer {
+
+    private final File file;
+
+    public LexicalAnalyzer(File file) {
+        this.file = file;
+    }
+
+    public void run() throws IOException {
+        String line;
+        String fileInfo = "";
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        while((line = br.readLine()) != null){
+            fileInfo += line;
+        }
+
+        //DFA dfa = new DFA(fileInfo);
+
+        br.close();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        if(args.length == 0){
+            System.out.println("파일이 없습니다");
+            System.exit(2);
+        }
+        File f = new File(args[0]);
+
+
+
     }
 }
