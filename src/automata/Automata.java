0922bcf168112e0f10e84002ee4db4bd18b58946
JangHoonSeok <orirori512@naver.com>
2022-04-28 18:26:56 +0900

    Complete DFAList

--- src/automata/Automata.java
+++ src/automata/Automata.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class Automata {
     private final static Transition transition = new Transition();
-    private final DFA[] DFAList = new DFA[16];
-    private final String[] insName = {"IF", "ELSE",""}
+    private final DFA[] DFAList = new DFA[18];
+    private final String[] insName = {"IF", "ELSE","WHILE","RETURN","VTYPE","INTEGER","STRING","OP","COMPARISON","ASSIGN","SEMI","LBRACE","RBRACE","LPAREN","RPAREN","COMMA","WHITESPACE","ID"};
     public Automata() {
         for (int i=0;i< insName.length;i++){
             DFAList[i] = new DFA(insName[i]);
